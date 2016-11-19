351be7dfa00ddf1d6cdc161b2255e021c6ffcb3e
johnhu <huzichunjohn@126.com>
2016-11-19 18:21:10 +0800

    add asynchronous task.

--- myshop/settings.py
+++ myshop/settings.py
@@ -109,3 +109,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+#BROKER_URL = 'redis://localhost:6379'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#CELERY_ACCEPT_CONTENT = ['application/json']
+#CELERY_TASK_SERIALIZER = 'json'
+#CELERY_RESULT_SERIALIZER = 'json'
+#CELERY_TIMEZONE = 'Asia/Shanghai'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
